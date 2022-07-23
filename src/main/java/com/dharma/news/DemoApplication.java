7d25c04b1ccd305c7e9ee74b80102f21e34d4de5
rowan <rowanfoo@gmail.com>
2022-07-23 22:38:02 +0800

    feature: run by commandline

--- src/main/java/com/dharma/news/DemoApplication.java
+++ src/main/java/com/dharma/news/DemoApplication.java
@@ -1,26 +1,32 @@
 package com.dharma.news;
 
+import com.dhamma.pesistence.entity.data.Portfolio;
+import com.dhamma.pesistence.entity.repo.PortfolioRepo;
+import com.dharma.news.data.entity.QNews;
 import com.dharma.news.data.repo.NewsRepo;
 import com.dharma.news.service.ElasticNewsService;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import lombok.extern.java.Log;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.File;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.time.LocalDateTime;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 
@@ -32,18 +38,26 @@ public class DemoApplication implements CommandLineRunner {
     private String seleniumpath;
     @Value("${holidays}")
     String holidays;
+    @Autowired
+    NewsRepo newsRepo;
+    @Autowired
+    ElasticNewsService elasticNewsService;
+    @Autowired
+    private ApplicationContext context;
+
+    static String arg = null;
 
     @Bean
     public WebDriver webdriver() {
         File src = new File(seleniumpath);
 
         System.out.println("START  AsxNewsChromeBrowser: ");
-        System.setProperty("webdriver.chrome.driver", src.getAbsolutePath());
+        System.out.println("-------CHROMW AOURCW-" + src.getAbsolutePath());
 
         WebDriver driver;
         ChromeOptions ChromeOptions = new ChromeOptions();
         ChromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox", "--disable-gpu");
-
+        ChromeOptions.setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe");
 
         driver = new ChromeDriver(ChromeOptions);
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS); // just after you have initiated browser
@@ -66,47 +80,7 @@ public class DemoApplication implements CommandLineRunner {
 
     }
 
-//    @Value("${selenium.weburl}")
-//    String weburl;
-//
-//    @Bean
-//    public WebDriver webdriver() {
-////        DesiredCapabilities dcap = DesiredCapabilities.chrome();
-////        String driverPath = System.getProperty("user.dir") + "/exe/chromedriver";
-////        System.setProperty("webdriver.chrome.driver", driverPath);
-//
-//        // You should check the Port No here.
-//
-//        DesiredCapabilities dcap = DesiredCapabilities.chrome();
-//
-//        final ChromeOptions chromeOptions = new ChromeOptions();
-//        chromeOptions.addArguments("-no-sandbox");
-//        chromeOptions.addArguments("-window-size=1024,768");
-//        dcap.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-//
-//
-//        URL gamelan = null;
-//        try {
-//            System.out.println("------------------" + weburl);
-//            gamelan = new URL(weburl);
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        }
-//        WebDriver driver = new RemoteWebDriver(gamelan, dcap);
-//        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-////        // Get URL
-////        driver.get("https://www.google.com/");
-////        // Print Title
-////        System.out.println(driver.getTitle());
-//
-//        return driver;
-//    }
 
-
-    @Autowired
-    NewsRepo newsRepo;
-
-    static String arg = null;
 
     public static void main(String[] args) {
 
@@ -115,63 +89,35 @@ public class DemoApplication implements CommandLineRunner {
             arg = args[0];
         }
 
-        //        SpringApplication.run(DemoApplication.class, args);
-        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(DemoApplication.class)
-                .web(WebApplicationType.NONE).run();
+        SpringApplication.run(DemoApplication.class, args);
+//        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(DemoApplication.class).web(WebApplicationType.NONE).run();
 
 
         System.out.println("--------------run2----------");
 
-        //SpringApplication.exit(ctx, () -> 0);
 
     }
 
-    //    @Autowired
-//    WebDriver webdriver;
-    @Autowired
-    ElasticNewsService elasticNewsService;
+
+
 
     @Override
     public void run(String... args) throws Exception {
 
-        elasticNewsService.importnewsbydate();
-//        Unirest.get("http://192.168.0.10:10100/scheduler/rowan").asJson();
+        if (arg != null) {
+            log.info("--------------RUN IMPORT PAGE NO--------------" + arg);
+
+            elasticNewsService.importnewsbypage(Integer.parseInt(arg));
+        } else {
+            log.info("--------------RUN IMPORT FILE NOW--------------" + LocalDateTime.now());
+            elasticNewsService.importnewsbydate();
+            log.info("--------------RUN IMPORT FILE NOW-----OK!!!!---------");
+
+        }
+        Unirest.get("http://192.168.0.10:10100/scheduler/rowan").asJson();
+        System.exit(SpringApplication.exit(context, () -> 0));
 
 
-        //        http://localhost:9000/importpage?page=9
-        //      elasticNewsService.importnewsbypage(9);
-//running code.
-//        if (arg != null) {
-//            log.info("--------------RUN IMPORT PAGE NO--------------" + arg);
-//
-//            elasticNewsService.importnewsbypage(Integer.parseInt(arg));
-//        } else {
-//            log.info("--------------RUN IMPORT FILE NOW--------------" + LocalDateTime.now());
-//            elasticNewsService.importnewsbydate();
-//            log.info("--------------RUN IMPORT FILE NOW-----OK!!!!---------");
-//
-//        }
-
-
-//        webdriver.get("https://hotcopper.com.au/");
-//        System.out.println("----- URLS   -------" + webdriver.getPageSource());
-//
-//        webdriver.get("https://hotcopper.com.au/announcements/asx/page-1/");
-//        System.out.println("----- URLS   -------" + webdriver.getPageSource());
-
-//        webdriver.get("https://hotcopper.com.au/announcements/asx/page-0/");
-//        System.out.println("----- URLS   -------" + webdriver.getPageSource());
-
-//        List<News> news = newsRepo.findAll();
-//        news.forEach(a -> {
-//
-//                    String mycode = a.getCode().substring(0, a.getCode().indexOf("."));
-//                    System.out.println("===================" + mycode);
-//                    a.setCode(mycode + ".AX");
-//                    newsRepo.save(a);
-//                }
-//
-//        );
 
     }
 
